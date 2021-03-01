066144c00340bdfec1d89e32aad6fc99bc07cf9b
yacut <roman.rogozhnikov@gmail.com>
2021-03-01 14:51:01 +0100

    fix crds

--- crds.tf
+++ crds.tf
@@ -2,45 +2,49 @@ resource "kubernetes_manifest" "incidents" {
   provider = kubernetes-alpha
 
   manifest = {
-    "apiVersion" = "apiextensions.k8s.io/v1beta1"
-    "kind"       = "CustomResourceDefinition"
-    "metadata" = {
-      "name" = "incidents.ilert.com"
+    apiVersion = "apiextensions.k8s.io/v1beta1"
+    kind       = "CustomResourceDefinition"
+    metadata = {
+      name = "incidents.ilert.com"
     }
-    "spec" = {
-      "group" = "ilert.com"
-      "scope" = "Namespaced"
+    spec = {
+      group = "ilert.com"
+      scope = "Namespaced"
 
-      "versions" = {
-        "name"    = "v1"
-        "served"  = true
-        "storage" = true
+      version = "v1"
+      versions = [{
+        name    = "v1"
+        served  = true
+        storage = true
+      }]
+
+      names = {
+        plural   = "incidents"
+        singular = "incident"
+        listKind = "IncidentList"
+        kind     = "Incident"
       }
 
-      "names" = {
-        "plural"   = "incidents"
-        "singular" = "incident"
-        "listKind" = "IncidentList"
-        "kind"     = "Incident"
-      }
-
-      "validation" = {
-        "openAPIV3Schema" = {
-          "required" = ["id"]
-          "properties" = {
-            "spec" = {
-              "id" = {
-                "type"    = "integer"
-                "minimum" = 0
-              }
-              "summary" = {
-                "type" = "string"
-              }
-              "details" = {
-                "type" = "string"
-              }
-              "type" = {
-                "type" = "string"
+      validation = {
+        openAPIV3Schema = {
+          required = ["spec"]
+          properties = {
+            spec = {
+              required = ["id"]
+              properties = {
+                id = {
+                  type    = "integer"
+                  minimum = 0
+                }
+                summary = {
+                  type = "string"
+                }
+                details = {
+                  type = "string"
+                }
+                type = {
+                  type = "string"
+                }
               }
             }
           }
