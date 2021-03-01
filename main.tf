9c951a5c033d6fb4b6513ad71a64e257e4fa9f1a
yacut <roman.rogozhnikov@gmail.com>
2021-03-01 14:25:06 +0100

    add crds

--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
 terraform {
   required_version = ">= 0.12"
+
+  required_providers {
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+
+    kubernetes-alpha = {
+      source  = "hashicorp/kubernetes-alpha"
+      version = ">= 0.2.1"
+    }
+  }
 }
