8b97522579c550664cb6cbe5ada3241ef9377059
yacut <roman.rogozhnikov@gmail.com>
2021-03-07 17:36:21 +0100

    upgrade

--- variables.tf
+++ variables.tf
@@ -30,7 +30,7 @@ variable "port" {
 variable "image" {
   description = "The ilert-kube-agent image to use. See https://github.com/iLert/ilert-kube-agent/releases for available versions"
   type        = string
-  default     = "ilert/ilert-kube-agent:v1.5.0"
+  default     = "ilert/ilert-kube-agent:v1.7.0"
 }
 
 variable "pod_annotations" {
@@ -94,6 +94,10 @@ variable "config" {
     })
 
     alarms = object({
+      cluster = object({
+        enabled  = bool
+        priority = string
+      })
       pods = object({
         enabled = bool
         terminate = object({
@@ -146,7 +150,7 @@ variable "config" {
     })
 
     links = object({
-      pods = map(string)
+      pods  = map(string)
       nodes = map(string)
     })
   })
@@ -162,6 +166,10 @@ variable "config" {
     }
 
     alarms = {
+      cluster = {
+        enabled  = true
+        priority = "HIGH"
+      }
       pods = {
         enabled = true
         terminate = {
@@ -220,7 +228,7 @@ variable "config" {
     }
 
     links = {
-      pods = {}
+      pods  = {}
       nodes = {}
     }
   }
